c1ff6b1777e8d520ae1bd52d3c0556290583abcc
Xavier Hueber <xavier.hueber@laposte.net>
2020-10-26 16:29:14 +0100

    Initial Commit

---
+++ TP1_Ex5.2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TP1_Ex5.2
+//
+//  Created by Xavier Hueber on 26/10/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
